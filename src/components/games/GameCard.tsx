989f81d29b6fa638d43de1d7d14b0e98a091cc23
Waruna Udayanga <warunaudayangalakruwan@gmail.com>
2024-07-06 04:40:21 +0530

    stable

--- src/components/games/GameCard.tsx
+++ src/components/games/GameCard.tsx
@@ -15,20 +15,22 @@ export const GameCard = ({ game }: Props): React.JSX.Element => {
     return (
         <Card h="100%">
             <Image src={getCroppedImageUrl(game.background_image) || gameSvg}></Image>
-            <Flex
-                position="absolute"
-                w="50px"
-                h="50px"
-                borderRadius="50%"
-                top={1}
-                right={3}
-                bg={"black"}
-                opacity={0.7}
-                alignItems="center"
-                justifyContent="center"
-            >
-                <Emoji rating={game.rating_top}></Emoji>
-            </Flex>
+            {game.rating_top > 3 && (
+                <Flex
+                    position="absolute"
+                    w="50px"
+                    h="50px"
+                    borderRadius="50%"
+                    top={1}
+                    right={3}
+                    bg={"black"}
+                    opacity={0.7}
+                    alignItems="center"
+                    justifyContent="center"
+                >
+                    <Emoji rating={game.rating_top}></Emoji>
+                </Flex>
+            )}
             <CardBody>
                 <VStack h="100%">
                     <HStack w="100%" justifyContent="space-between">
